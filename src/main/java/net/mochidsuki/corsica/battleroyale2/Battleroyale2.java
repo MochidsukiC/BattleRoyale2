434272edbdf5a20b88c2ee05855bafbb063553e5
BuildTools <unconfigured@null.spigotmc.org>
2022-12-06 21:25:00 +0900

    Initial commit

---
+++ src/main/java/net/mochidsuki/corsica/battleroyale2/Battleroyale2.java
@@ -0,0 +1,17 @@
+package net.mochidsuki.corsica.battleroyale2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Battleroyale2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
