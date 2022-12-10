d0e69e9fb4e0e723d013d11f8828cc1433b3f07c
BuildTools <unconfigured@null.spigotmc.org>
2022-12-10 21:03:18 +0900

    22_12_10 from PC

--- src/main/java/net/mochidsuki/corsica/battleroyale2/Battleroyale2.java
+++ src/main/java/net/mochidsuki/corsica/battleroyale2/Battleroyale2.java
@@ -1,8 +1,5 @@
 package net.mochidsuki.corsica.battleroyale2;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Battleroyale2 extends JavaPlugin {
