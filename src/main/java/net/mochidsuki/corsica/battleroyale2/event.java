e032aa591cc1e40da5d8d7ee22348b74527a3070
BuildTools <unconfigured@null.spigotmc.org>
2022-12-11 19:25:01 +0900

    22_12_11 from PC

--- src/main/java/net/mochidsuki/corsica/battleroyale2/event.java
+++ src/main/java/net/mochidsuki/corsica/battleroyale2/event.java
@@ -1,8 +1,17 @@
 package net.mochidsuki.corsica.battleroyale2;
 
+
+import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class event implements Listener{
+    @EventHandler
+    public void EntityDropItemEvent(Entity entity, Item item){
+    if(item = ){
+
+    }
+    }
 
 
 }
